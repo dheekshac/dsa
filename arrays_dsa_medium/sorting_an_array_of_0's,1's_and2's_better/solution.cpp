951c00ac475f6611ac810db922954411971b54b6
dheekshac <dheekshac07@gmail.com>
2025-09-25 20:52:55 +0530

    Create solution.cpp
    #include <bits/stdc++.h>
    using namespace std;
    void array_of012(int arr[],int n){
        int count0=0,count1=0,count2=0;
        for(int i=0;i<n;i++){
            if(arr[i]==0) count0++;
            if(arr[i]==1) count1++;
            if(arr[i]==2) count2++;
        }
        for(int i=0;i<count0;i++){
            arr[i]=0;
        }
        for(int i=count0;i<count0+count1;i++){
            arr[i]=1;
        }
        for(int i=count0+count1;i<n;i++){
            arr[i]=2;
        }
        for(int it=0;it<n;it++){
            cout<<arr[it]<<' ';
        }
    }
    
    int main() {
        int n;cin>>n;
        int arr[n];
        for(int i=0;i<n;i++){
            cin>>arr[i];
        }
        array_of012(arr,n);
    }

---
+++ arrays_dsa_medium/sorting_an_array_of_0's,1's_and2's_better/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+void array_of012(int arr[],int n){
+    int count0=0,count1=0,count2=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==0) count0++;
+        if(arr[i]==1) count1++;
+        if(arr[i]==2) count2++;
+    }
+    for(int i=0;i<count0;i++){
+        arr[i]=0;
+    }
+    for(int i=count0;i<count0+count1;i++){
+        arr[i]=1;
+    }
+    for(int i=count0+count1;i<n;i++){
+        arr[i]=2;
+    }
+    for(int it=0;it<n;it++){
+        cout<<arr[it]<<' ';
+    }
+}
+
+int main() {
+    int n;cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    array_of012(arr,n);
+}
