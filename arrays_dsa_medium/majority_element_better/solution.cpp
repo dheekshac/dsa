3d59b507332170b213bbeb5f4cb8a8c60aefb7a6
dheekshac <dheekshac07@gmail.com>
2025-09-26 23:39:09 +0530

    Create solution.cpp
    #include <bits/stdc++.h>
    using namespace std;
    int majority_element(vector<int>arr,int n){
        unordered_map<int,int>mpp;
        for(int i=0;i<n;i++){
            mpp[arr[i]]++;
        }
        for(auto it:mpp){
            if(it.second>n/2){
                return it.first;
            }
        }
        return -1;
    }
    int main(){
    	int n;cin>>n;
    	 vector<int>arr(n);
    	for(int i=0;i<n;i++){
    	    cin>>arr[i];
    	}
        int result = majority_element(arr,n);
        cout<<result<<endl;
    }

---
+++ arrays_dsa_medium/majority_element_better/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int majority_element(vector<int>arr,int n){
+    unordered_map<int,int>mpp;
+    for(int i=0;i<n;i++){
+        mpp[arr[i]]++;
+    }
+    for(auto it:mpp){
+        if(it.second>n/2){
+            return it.first;
+        }
+    }
+    return -1;
+}
+int main(){
+	int n;cin>>n;
+	 vector<int>arr(n);
+	for(int i=0;i<n;i++){
+	    cin>>arr[i];
+	}
+    int result = majority_element(arr,n);
+    cout<<result<<endl;
+}
